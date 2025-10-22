857489ac87b12ed5dc15d0095d13c95df8fc0e0f
Amir Mousavi <amirhossein.mou@emtnirancell.ir>
2025-10-22 12:49:49 +0330

    feat: Initial project setup

---
+++ app/src/main/java/com/amirmousavi_dev/persian_date_picker/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.amirmousavi_dev.persian_date_picker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.amirmousavi_dev.persian_date_picker.ui.theme.Persian_Date_PickerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Persian_Date_PickerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Persian_Date_PickerTheme {
+        Greeting("Android")
+    }
+}
